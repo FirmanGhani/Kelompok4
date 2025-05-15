3239b5322aed0a40ff4f7041087decbc333d8aa3
Mutiara Hasanah <hasanahmutiara05@gmail.com>
2025-05-15 23:01:17 +0700

    try

--- logout.php
+++ logout.php
@@ -1,16 +1,27 @@
-<?php
+<?php 
+ob_start();
 session_start();
 
-// Hapus semua session variables, destroy the session, lalu redirect ke halaman login
-// Hapus semua session variables
-session_unset();
+//includes data base config
+include('dbconfig.php');
 
-// Hancurkan sesi
+//Unset the session variable 
+unset($_SESSION['user']);
+
+//destroy created session
 session_destroy();
 
-// Redirect ke halaman login
-header("Location: login.php");
+// Redirect to login page
+header("location: login.php");
 
-
+//check if session is expired
+if(isset($_GET["session_expire"])) {
+	$url= "login.php?session_expire=" . $_GET["session_expire"];
+	//Destroy session if time's out
+	unset($_SESSION['user']);
+	//redirect to login if expired
+	 header("Location: login.php");
+	 
+}
 exit;
-?> 
+?>
